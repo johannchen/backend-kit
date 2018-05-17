684935061537d431458431843356d4493d12d199
Johann Chen <johannchen@gmail.com>
2018-05-16 23:37:27 -0700

    setup test db

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,16 @@ import 'reflect-metadata';
 import { GraphQLServer } from 'graphql-yoga';
 import { importSchema } from 'graphql-import';
 import { resolvers } from './resolvers';
-import { createConnection } from 'typeorm';
 import * as path from 'path';
+import { createTypeormConn } from './utils/createTypeormConn';
 
-const typeDefs = importSchema(path.join(__dirname, 'schema.graphql'));
-const server = new GraphQLServer({ typeDefs, resolvers });
-createConnection().then(() => {
-  server.start(() => console.log('Server is running on localhost:4000'));
-});
+export const startServer = async () => {
+  const typeDefs = importSchema(path.join(__dirname, './schema.graphql'));
+
+  const server = new GraphQLServer({ typeDefs, resolvers });
+  await createTypeormConn();
+  await server.start();
+  console.log('Server is running on localhost:4000');
+};
+
+startServer();
