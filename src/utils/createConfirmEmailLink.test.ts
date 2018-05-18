643cf906a674742a651af0bed4ddb8103844fa34
Johann Chen <johannchen@gmail.com>
2018-05-18 11:12:47 -0700

    clear key in redis

--- src/utils/createConfirmEmailLink.test.ts
+++ src/utils/createConfirmEmailLink.test.ts
@@ -27,4 +27,11 @@ test('createConfirmEmailLink and clears key in redis', async () => {
   const response = await fetch(url);
   const text = await response.text();
   expect(text).toEqual('ok');
+  const user = await User.findOne(userId);
+  expect((user as User).confirmed).toBeTruthy();
+  // clear key in redis
+  const chunks = url.split('/');
+  const key = chunks[chunks.length - 1];
+  const value = await redis.get(key);
+  expect(value).toBeNull();
 });
