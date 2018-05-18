3179ea5289f25a59ca2423f1948a85c94d4ecaed
Johann Chen <johannchen@gmail.com>
2018-05-18 11:41:25 -0700

    fix connection on test watch

--- src/test-setup/callSetup.js
+++ src/test-setup/callSetup.js
@@ -5,6 +5,9 @@ const { setup } = require('./setup');
 
 module.exports = async function() {
   // Call your initialization methods here.
-  await setup();
+  // avoid duplicate setup on test watch
+  if (!process.env.TEST_HOST) {
+    await setup();
+  }
   return null;
 };
